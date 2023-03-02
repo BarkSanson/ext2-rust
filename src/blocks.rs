9873239ee93e0f634fb46a03ad98837659b6bbd3
bark <barksansondev@gmail.com>
2023-03-02 14:28:46 +0100

    Created some tests for `blocks.rs`

--- src/blocks.rs
+++ src/blocks.rs
@@ -17,49 +17,104 @@
 /// As you can see, this is a bitmap and inode array implementation, since this
 /// FS is inspired in the ext2 FS.
 
-use std::{fs, fs::File, io::{self}, os::unix::prelude::FileExt};
+use std::{fs, fs::File, io::{self}};
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::os::unix::fs::PermissionsExt;
 
 pub const BLOCKSIZE: usize = 1024;
 
-pub struct BlockManager {
-    v_device: File,
+pub struct BlockManager<T: Read + Write + Seek> {
+    v_device: T,
     size: usize
 }
 
-impl BlockManager {
+impl BlockManager<File> {
     pub fn new(path: &str) -> Result<Self, io::Error> {
         let v_device = File::create(path)?;
         v_device.set_permissions(fs::Permissions::from_mode(0o666))
             .expect("TODO: panic message");
 
-        Ok (Self { v_device, size: 0 })
+        Ok(Self { v_device, size: 0 })
+    }
+}
+
+impl<T: Read + Write + Seek> BlockManager<T> {
+    fn from_in_memory(buffer: T) -> Self {
+        Self { v_device: buffer, size: 0}
     }
 
-    pub fn write_block(&mut self, block_number: u64, buffer: &[u8; BLOCKSIZE as usize]) -> Result<usize, io::Error> {
+    pub fn write_block(&mut self, block_number: usize, buffer: &[u8; BLOCKSIZE]) -> Result<usize, io::Error> {
         let offset = BLOCKSIZE * block_number;
 
-        let bytes_written = self.v_device.write_at(buffer, offset as u64)?;
+        self.v_device.seek(SeekFrom::Start(offset as u64))?;
+
+        let bytes_written = self.v_device.write(buffer)?;
         self.size += bytes_written;
+
+        self.v_device.rewind()?;
 
         Ok(bytes_written)
     }
 
-    pub fn read_block(&self, block_number: u64, buffer: &mut [u8; BLOCKSIZE as usize]) -> Result<(), io::Error> {
+    pub fn read_block(&mut self, block_number: usize, buffer: &mut [u8; BLOCKSIZE]) -> Result<(), io::Error> {
         let offset = BLOCKSIZE * block_number;
 
-        self.v_device.read_exact_at(buffer, offset as u64)?;
+        self.v_device.seek(SeekFrom::Start(offset as u64))?;
+
+        self.v_device.read_exact(buffer)?;
+
+        self.v_device.rewind()?;
 
         Ok(())
     }
 
 }
 
-// TODO: develop tests for this module
-//#[cfg(test)]
-//mod tests {
-//    use std::fs;
-//    use std::path::Path;
-//    use crate::blocks::BlockManager;
-//    use fs::remove_file;
-//}
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+    use crate::blocks::BLOCKSIZE;
+    use super::BlockManager;
+
+    #[test]
+    fn correct_bytes_are_written() {
+        let buffer = Cursor::new(Vec::new());
+        let mut block_manager = BlockManager::from_in_memory(buffer);
+        let data: [u8; BLOCKSIZE] = [0xFF; BLOCKSIZE];
+        let expected = BLOCKSIZE;
+        let block_number = 0;
+
+        let actual = block_manager.write_block(block_number, &data).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn correct_bytes_are_read() {
+        let buffer = Cursor::new(Vec::new());
+        let mut block_manager = BlockManager::from_in_memory(buffer);
+
+        // Write different data in each consecutive block
+        for i in 0..4 as usize {
+            let data: [u8; BLOCKSIZE];
+            if i % 2 == 0{
+                data = [0xFF; BLOCKSIZE];
+            } else {
+                data = [0x00; BLOCKSIZE];
+            }
+            block_manager.write_block(i, &data).unwrap();
+        }
+
+        // Each consecutive block should have the same data written before
+        for i in 0..4 as usize {
+            let mut actual: [u8; BLOCKSIZE] = [0x66; BLOCKSIZE];
+            block_manager.read_block(i, &mut actual).unwrap();
+
+            if i % 2 == 0 {
+                assert_eq!(actual, [0xFF; BLOCKSIZE]);
+            } else {
+                assert_eq!(actual, [0x00; BLOCKSIZE]);
+            }
+        }
+    }
+}
